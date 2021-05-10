e708df77a4035784680bb108b824e4e88aff583c
Tru-Dev <29785998+Tru-Dev@users.noreply.github.com>
2021-05-10 14:25:44 -0700

    Got rid of Weather Service in Startup.cs

--- RetroGG.net/Startup.cs
+++ RetroGG.net/Startup.cs
@@ -40,7 +40,6 @@ namespace RetroGG.net
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
